08d4bc1bfcfca63f57ddcfed0993f0cbf5d31dfb
Keith Stolte <keithrstolte@gmail.com>
2022-09-07 08:29:35 -0400

    build: adjusting usage of google analytics for v4

--- src/util/analytics.js
+++ src/util/analytics.js
@@ -1,20 +1,20 @@
-import Analytics from "analytics";
-import googleAnalyticsPlugin from "@analytics/google-analytics";
-import { history } from "./router.js";
+import Analytics from 'analytics';
+import googleAnalyticsPlugin from '@analytics/google-analytics';
+import { history } from './router.js';
 
 // Initialize analytics and plugins
 // Documentation: https://getanalytics.io
 const analytics = Analytics({
-  debug: process.env.NODE_ENV !== "production",
+  debug: process.env.NODE_ENV !== 'production',
   plugins: [
     googleAnalyticsPlugin({
-      trackingId: process.env.REACT_APP_GA_TRACKING_ID,
+      measurementIds: [process.env.REACT_APP_GA_TRACKING_ID],
     }),
   ],
 });
 
 // Track initial pageview
-if (typeof window !== "undefined") {
+if (typeof window !== 'undefined') {
   analytics.page();
 }
 
