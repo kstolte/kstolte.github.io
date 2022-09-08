280eff9702f82f7e9684e00078d1d1de82a61a9a
Keith Stolte <keithrstolte@gmail.com>
2022-09-07 22:16:42 -0400

    ui(resume): adjusting button to have appropriate color name
    this is more than likely the issue that is causing the button to not render appropriately on live site.

--- src/pages/resume-cv.js
+++ src/pages/resume-cv.js
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useCallback } from 'react';
-import { Button, Container } from '@mui/material';
+import { Button } from '@mui/material';
 
 import cvRecords from '../providers/data/cv.json';
 
@@ -23,17 +23,16 @@ function ResumeCvPage(_props) {
   return (
     <>
       <Meta title="Résume / CV - Keith Stolte" />
-      <Container>
-        <Button
-          variant="outlined"
-          onClick={showLongShort}
-          // endIcon={<LoupeIcon />}
-          fullWidth={true}
-        >
-          {buttonText}
-        </Button>
-        <Resume cv={fullCV ? cvRecords : resume} />
-      </Container>
+      <Button
+        variant="outlined"
+        color="primary"
+        size="large"
+        onClick={showLongShort}
+        fullWidth={true}
+      >
+        {buttonText}
+      </Button>
+      <Resume cv={fullCV ? cvRecords : resume} />
     </>
   );
 }
