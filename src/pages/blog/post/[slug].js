c76f9a71cb312af2e02e756d856d5eec5c6d90b5
Keith Stolte <keithrstolte@gmail.com>
2023-02-24 08:54:49 -0500

    minor adjustments to the cv and blog template

--- src/pages/blog/post/[slug].js
+++ src/pages/blog/post/[slug].js
@@ -30,7 +30,7 @@ export default function Post({ post, morePosts, preview }) {
         <>
           <article className="mb-32">
             <Head>
-              <title>{post.title} | Next.js Blog Example with</title>
+              <title>{post.title} | KeithStolte.io | Blog</title>
               <meta property="og:image" content={post.ogImage.url} />
             </Head>
             <HeroSection2
